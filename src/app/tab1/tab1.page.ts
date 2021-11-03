d6f273fc8f47d378bb93b7dacdec02fee389a8fd
Diego Chagas das Neves <respeixe@gmail.com>
2021-11-03 01:12:46 -0300

    Adicionamos o array de filmes e a interface de filme

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,3 +1,4 @@
+import { IFilme } from '../models/IFilme.model';
 import { Component } from '@angular/core';
 import { AlertController } from '@ionic/angular';
 import { ToastController } from '@ionic/angular';
@@ -9,6 +10,27 @@ import { ToastController } from '@ionic/angular';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+
+    titulo = 'Videos';
+
+    listaVideos: IFilme[] = [
+      {
+        nome: 'A Profissional (2021)',
+        lancamento: '11/11/2021',
+        duracao: '1h 49m',
+        classificacao: 76,
+        cartaz: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/niDrDAUYO8627hwEhE7LV5Z1qXn.jpg',
+        generos: ['Ação', 'Crime', 'Thriller']
+      },
+      {
+        nome: 'Assalto ao Banco da Espanha (2021)',
+        lancamento: '26/03/2021',
+        duracao: '1h 58m',
+        classificacao: 76,
+        cartaz: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/AhRSv4npEaE8ZvuJh4CmQpsXDZg.jpg',
+        generos: ['Crime', 'Ação', 'Thriller']
+      }
+    ];
 
   constructor(public alertController: AlertController, public toastController: ToastController) {}
 
